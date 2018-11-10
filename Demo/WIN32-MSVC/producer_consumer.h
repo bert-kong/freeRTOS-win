f2b4929b2ef81af56ccaaeaa4faf728d809b75ef
U-INOVIO\bert.kong <bert.kong@inovio.com>
2018-11-10 08:20:58 -0800

    original source code

---
+++ Demo/WIN32-MSVC/producer_consumer.h
@@ -0,0 +1,18 @@
+#ifndef __PRODUCER_CONSUMER_H__
+#define __PRODUCER_CONSUMER_H__
+
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#if 0
+struct _ProducerConsumerStruct
+{
+	TaskHandle_t handle;
+	xQueueHandle queue;
+}
+#endif
+
+
+//template<typename T>
+#endif // !__PRODUCER_CONSUMER_H__
